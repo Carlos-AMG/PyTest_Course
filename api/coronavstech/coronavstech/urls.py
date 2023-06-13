7702bbded19276aa46b0cf0b982e945e4fa4d194
Carlos-AMG <alberto-cmg@outlook.es>
2023-06-13 14:45:49 -0600

    refactor companies files, gitignore and add pytest.ini

--- api/coronavstech/coronavstech/urls.py
+++ api/coronavstech/coronavstech/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from companies.urls import companies_router
-# from api.coronavstech.companies.urls import companies_router
+
+# from companies.urls import companies_router
+from api.coronavstech.companies.urls import companies_router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include(companies_router.urls))
+    path("", include(companies_router.urls)),
 ]
