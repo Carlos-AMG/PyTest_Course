a4c0aac84416b96eef77c4bc8ea92d964904fc88
Carlos-AMG <alberto-cmg@outlook.es>
2023-06-16 08:07:11 -0600

    refactor test_emails to PyTest Syntax

--- api/coronavstech/companies/tests/test_emails.py
+++ api/coronavstech/companies/tests/test_emails.py
@@ -1,46 +1,44 @@
 import json
-from django.test import Client, TestCase
+
+# from django.test import Client
 from django.core import mail
 from unittest.mock import patch
 
-class EmailUnitTest(TestCase):
-    def test_send_email_should_succeed(self) -> None:
-        with self.settings(
-            EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend'
-        ):
-            
-            self.assertEqual(len(mail.outbox), 0)
 
-            mail.send_mail(
-                subject="Test Subject here",
-                message="Test Here is the message",
-                from_email="testemail@gmail.com",
-                recipient_list=["testemail2@gmail.com"],
-                fail_silently=False
-            )
+def test_send_email_should_succeed(mailoutbox, settings) -> None:
+    settings.EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+    assert len(mailoutbox) == 0
 
-            self.assertEqual(len(mail.outbox), 1)
-            self.assertEqual(mail.outbox[0].subject, "Test Subject here")
+    mail.send_mail(
+        subject="Test Subject here",
+        message="Test Here is the message",
+        from_email="testemail@gmail.com",
+        recipient_list=["testemail2@gmail.com"],
+        fail_silently=False,
+    )
 
-    def test_send_email_without_arguments_should_send_empty_email(self) -> None:
-        client = Client()
-        with patch(
-            "api.coronavstech.companies.views.send_mail"
-        ) as mocked_send_mail_function :
-            response = client.post(path="/send-email")
-            response_content = json.loads(response.content)
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response_content['status'], 'success')
-            self.assertEqual(response_content['info'], 'email sent successfully')
-            mocked_send_mail_function.assert_called_with(
-                subject=None,
-                message=None,
-                from_email="carlosaamg6@gmail.com",
-                recipient_list=["carlosaamg6@gmail.com"]
-            )
+    assert len(mailoutbox) == 1
+    assert mailoutbox[0].subject == "Test Subject here"
 
-    def test_send_email_with_get_verb_should_fail(self) -> None:
-        client = Client()
-        response = client.get(path="/send-email")
-        self.assertEqual(response.status_code, 405)
-        self.assertEqual(json.loads(response.content), {"detail":"Method \"GET\" not allowed."})
+
+def test_send_email_without_arguments_should_send_empty_email(client) -> None:
+    with patch(
+        "api.coronavstech.companies.views.send_mail"
+    ) as mocked_send_mail_function:
+        response = client.post(path="/send-email")
+        response_content = json.loads(response.content)
+        assert response.status_code == 200
+        assert response_content["status"] == "success"
+        assert response_content["info"] == "email sent successfully"
+        mocked_send_mail_function.assert_called_with(
+            subject=None,
+            message=None,
+            from_email="carlosaamg6@gmail.com",
+            recipient_list=["carlosaamg6@gmail.com"],
+        )
+
+
+def test_send_email_with_get_verb_should_fail(client) -> None:
+    response = client.get(path="/send-email")
+    assert response.status_code == 405
+    assert json.loads(response.content) == {"detail": 'Method "GET" not allowed.'}
--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -8,14 +8,22 @@ from rest_framework.response import Response
 from django.core.mail import send_mail
 from rest_framework.request import Request
 
+
 # Create your views here.
 class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all().order_by("-last_update")
     pagination_class = PageNumberPagination
 
+
 @api_view(http_method_names=["POST"])
-def send_company_email(request:Request):
-    send_mail(subject=request.data.get("subject"), message=request.data.get("message"), 
-              from_email="carlosaamg6@gmail.com", recipient_list=["carlosaamg6@gmail.com"])
-    return Response({"status":"success", "info":"email sent successfully"}, status=200)
+def send_company_email(request: Request):
+    send_mail(
+        subject=request.data.get("subject"),
+        message=request.data.get("message"),
+        from_email="carlosaamg6@gmail.com",
+        recipient_list=["carlosaamg6@gmail.com"],
+    )
+    return Response(
+        {"status": "success", "info": "email sent successfully"}, status=200
+    )
