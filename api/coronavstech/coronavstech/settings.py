f1ad312630627b18a9fc37f6fcadb9ce1f35ace1
Carlos-AMG <alberto-cmg@outlook.es>
2023-06-16 00:59:11 -0600

    add try to .config import so that actions doesn't crash

--- api/coronavstech/coronavstech/settings.py
+++ api/coronavstech/coronavstech/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from .config import *
+try:
+    from .config import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
